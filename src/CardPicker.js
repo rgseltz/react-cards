738de92dfdd1a3ed1416cd143ff70134b1cb24de
ryan <ryanjgecelter@gmail.com>
2022-08-10 17:08:23 +0300

    refactor: move api req for get card out of useEffect - only useEffect is to get initial deckId on mount

--- src/CardPicker.js
+++ src/CardPicker.js
@@ -1,57 +1,35 @@
 import React, {useState, useEffect} from "react";
 import Card from "./Card";
 import Axios from "axios";
+import {v4 as uuid} from 'uuid';
 
 const CardPicker = () => {
     const [deck, setDeck] = useState(null);
     const [cardUrl, setCardUrl] = useState(null)
     const [cards, setCards] = useState([])
-    const [getCard, setGetCard] = useState(false)
     
     /**Gets initial deck id */
     useEffect(() => {
         async function shuffle() {
-            const res = await Axios.get('http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+            const res = await Axios.get('http://deckofcardsapi.com/api/deck/new/shuffle/')
             setDeck(() => res.data.deck_id);
             };
         shuffle();
         console.log(cardUrl)
-    },[setDeck])
-    console.log(deck)
+    },[])
 
-    const handlePick = ()=> {
-        setCardUrl(`http://deckofcardsapi.com/api/deck/${deck}/draw`);
-        toggleGetCard();
-    }
-
-    const toggleGetCard = ()=> {
-        setGetCard(!getCard)
-    }
-    console.log(cardUrl)
-    /**Gets a new card object when handlePick is called */
-    useEffect(() => {
-        try {
-        if (getCard) {
-        async function pickCard() {
-            const cardRes = await Axios.get(cardUrl);
-            console.log(cardRes)
-            setCards([...cards, cardRes.data.cards[0]])
-        } 
-        pickCard();
-        return () => setGetCard(true)
-    } else return;
-    } catch (err) {
-        console.log('Request running too soon?')
-    }
-    },[getCard])
-    console.log(cards)
+    const pickCard = async () => {
+        const cardRes = await Axios.get(`http://deckofcardsapi.com/api/deck/${deck}/draw`);
+        console.log(cardRes)
+        setCards([...cards, cardRes.data.cards[0]])
+    } 
     
     return (
         <div>
         <h1>Card Picker!</h1>
         {/* <button onClick={shuffleDeck}>New Deck</button> */}
-        <button onClick={handlePick}>Pick Card</button>
-        {cards ? <section> {cards.map(c => <Card info={c}/>)}</section> : null}
+        <button onClick={pickCard}>Pick Card</button>
+        {cards ? <section> {cards.map(c => <Card key={c.code} info={c}/>)}</section> : null}
         </div>
     )
 }
