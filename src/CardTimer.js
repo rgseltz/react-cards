3d2f4df500a9f60184848ee47eb9232781f826bc
ryan <ryanjgecelter@gmail.com>
2022-08-10 15:04:13 +0300

    fix CardsTimer component - no return statement

--- src/CardTimer.js
+++ src/CardTimer.js
@@ -2,7 +2,9 @@ import React, {useState, useEffect, useRef} from "react";
 import Card from "./Card";
 
 const CardTimer = () => {
+    return (
     <h1>CardTimer!</h1>
+    )
 }
 
 export default CardTimer;
